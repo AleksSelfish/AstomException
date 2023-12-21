0e3bffb4c300f17446f6671358555bb392ed91dd
Alexandr Perov <101975023+AleksSelfish@users.noreply.github.com>
2023-12-22 00:49:47 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws MyException {
 
 
-        //Exeprion
+        //Exception
         //ex 1
         String string = null;
         try {
